c8396caf4bb51267a67f058b0c39772ce298f6b9
Jason Vasquez <fjvasquez@aol.com>
2014-10-10 14:24:04 -0700

    working now

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.get('/', function (req, res) {
 
 app.use('/js', express.static(__dirname + '/client/js'));
 
+//Rest API
+app.get('/api/meetups', meetupsController.list);
 app.post('/api/meetups', meetupsController.create);
 
 app.listen(3000, function() {
